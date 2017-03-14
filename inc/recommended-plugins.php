20094c48aaf952ed0edfc7632454b2a304d8d02f
Andrew Misplon <amisplon@gmail.com>
2017-03-14 15:42:57 +0200

    Add TGM

---
+++ inc/recommended-plugins.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This file represents an example of the code that themes would use to register
+ * the required plugins.
+ *
+ * It is expected that theme authors would copy and paste this code into their
+ * functions.php file, and amend to suit.
+ *
+ * @see http://tgmpluginactivation.com/configuration/ for detailed documentation.
+ *
+ * @package    TGM-Plugin-Activation
+ * @subpackage polestar
+ * @version    2.6.1 for parent theme Polestar for publication on WordPress.org
+ * @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
+ * @copyright  Copyright (c) 2011, Thomas Griffin
+ * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
+ * @link       https://github.com/TGMPA/TGM-Plugin-Activation
+ */
+
+/**
+ * Include the TGM_Plugin_Activation class.
+ */
+require_once get_template_directory() . '/inc/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'polestar_register_required_plugins' );
+
+/**
+ * Register the required plugins for this theme.
+ */
+function polestar_register_required_plugins() {
+
+	$plugins = array(
+
+		array(
+			'name'      => 'SiteOrigin Page Builder',
+			'slug'      => 'siteorigin-panels',
+			'required'  => false,
+		),
+
+		array(
+			'name'      => 'SiteOrigin Widgets Bundle',
+			'slug'      => 'so-widgets-bundle',
+			'required'  => false,
+		),		
+
+	);
+
+	$config = array(
+		'id'           => 'tgmpa-polestar',        // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
