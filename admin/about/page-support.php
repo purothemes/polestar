31ddf7642dfdbe68586abdba5ad24de32fed8435
Andrew Misplon <amisplon@gmail.com>
2017-03-17 08:14:12 +0200

    Update about page section

--- admin/about/page-support.php
+++ admin/about/page-support.php
@@ -1,7 +1,4 @@
-<h3><?php esc_html_e( 'Support and Documentation', 'polestar' ); ?></h3>
+<h3><?php esc_html_e( 'Support', 'polestar' ); ?></h3>
 <p>
 	<?php printf( esc_html__( "Our dedicated support team is ready to help you over on our %sfree support forums%s.", 'polestar' ), '<a href="https://purothemes.com/support/forum/polestar-theme/">', '</a>' ); ?>
-</p>
-<p>
-	<?php printf( esc_html__( "From theme basics to in-depth help articles, our %stheme documentation%s is ready.", 'polestar' ), '<a href="https://purothemes.com/documentation/polestar-theme/">', '</a>' ); ?>
 </p>
