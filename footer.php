6a7e6835281b3876d369f801cff033a20eae0714
Andrew Misplon <amisplon@gmail.com>
2017-03-17 08:16:44 +0200

    Add page settings conditionals to footer widgets

--- footer.php
+++ footer.php
@@ -10,6 +10,9 @@
  * @license GPL 2.0 
  */
 
+$footer_widgets = is_active_sidebar( 'sidebar-footer' ) == true;
+$footer_widgets_page_setting = puro_page_setting( 'footer_widgets', true );
+
 ?>
 
 		</div><!-- .polestar-container -->
@@ -17,22 +20,25 @@
 
 	<?php do_action( 'polestar_footer_before' ); ?>
 
-	<footer id="colophon" class="site-footer <?php if ( is_active_sidebar( 'sidebar-footer' ) ) echo 'footer-active-sidebar'; if ( get_theme_mod( 'footer_layout' ) == 'full-width' ) echo ' full-width';  ?>" role="contentinfo">
+	<footer id="colophon" class="site-footer <?php if ( ! is_page() && $footer_widgets || is_page() && ( $footer_widgets && $footer_widgets_page_setting ) ) echo 'footer-active-sidebar'; if ( get_theme_mod( 'footer_layout' ) == 'full-width' ) echo ' full-width';  ?>" role="contentinfo">
 
 		<?php do_action( 'polestar_footer_top' ); ?>
 
-		<div class="polestar-container">
-			<?php
-				if ( is_active_sidebar( 'sidebar-footer' ) ) {
-					$polestar_footer_sidebars = wp_get_sidebars_widgets();
-					?>
-					<div class="widgets widgets-<?php echo count( $polestar_footer_sidebars['sidebar-footer'] ) ?>" role="complementary" aria-label="<?php esc_html_e( 'Footer Widgets', 'polestar' ); ?>">
-						<?php dynamic_sidebar( 'sidebar-footer' ); ?>
-					</div>
-					<?php
-				}
-			?>
-		</div><!-- .polestar-container -->
+		<?php if ( ! is_page() || is_page() && $footer_widgets_page_setting ) : ?>
+			<div class="polestar-container">
+				<?php
+					if ( is_active_sidebar( 'sidebar-footer' ) ) {
+						$polestar_footer_sidebars = wp_get_sidebars_widgets();
+						?>
+						<div class="widgets widgets-<?php echo count( $polestar_footer_sidebars['sidebar-footer'] ) ?>" role="complementary" aria-label="<?php esc_html_e( 'Footer Widgets', 'polestar' ); ?>">
+							<?php dynamic_sidebar( 'sidebar-footer' ); ?>
+						</div>
+						<?php
+					}
+				?>
+			</div><!-- .polestar-container -->				
+		<?php endif; ?>	
+			
 		<div class="bottom-bar">
 			<div class="polestar-container">
 				<div class="site-info">
