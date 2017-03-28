63b9cdd77a6bd8547c788a60214d9455807e2bc9
Andrew Misplon <amisplon@gmail.com>
2017-03-28 21:50:15 +0200

    Add WooCommerce search widget styling

---
+++ woocommerce/product-searchform.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying product search form.
+ *
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.5.0
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+
+<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
+	<label class="screen-reader-text" for="woocommerce-product-search-field"><?php _e( 'Search for:', 'polestar' ); ?></label>
+	<input type="search" id="woocommerce-product-search-field" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'polestar' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'polestar' ); ?>" />
+	<button type="submit">
+		<label class="screen-reader-text"><?php esc_html_e( 'Search', 'polestar' ); ?></label>
+		<?php polestar_display_icon( 'search' ); ?>
+	</button>
+	<input type="hidden" name="post_type" value="product" />
+</form>
