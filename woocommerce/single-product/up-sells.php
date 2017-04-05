2522879e6ef1362f680bc71c1fdbaf2e0df5a9fa
Andrew Misplon <amisplon@gmail.com>
2017-04-05 10:36:41 +0200

    Template updates for WooCommerce 3.0

--- woocommerce/single-product/up-sells.php
+++ woocommerce/single-product/up-sells.php
@@ -2,63 +2,34 @@
 /**
  * Single Product Up-Sells
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product/up-sells.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
  * @version     1.6.4
  */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+if ( $upsells ) : ?>
 
-global $product, $woocommerce_loop;
+	<section class="up-sells upsells products">
 
-if ( ! $upsells = $product->get_upsells() ) {
-	return;
-}
-
-$args = array(
-	'post_type'           => 'product',
-	'ignore_sticky_posts' => 1,
-	'no_found_rows'       => 1,
-	'posts_per_page'      => $posts_per_page,
-	'orderby'             => $orderby,
-	'post__in'            => $upsells,
-	'post__not_in'        => array( $product->id ),
-	'meta_query'          => WC()->query->get_meta_query()
-);
-
-$products                    = new WP_Query( $args );
-$woocommerce_loop['name']    = 'up-sells';
-$woocommerce_loop['columns'] = apply_filters( 'woocommerce_up_sells_columns', $columns );
-
-if ( $products->have_posts() ) : ?>
-
-	<div class="up-sells upsells products">
-
-		<h2><?php _e( 'You may also like', 'polestar' ) ?></h2>
+		<h2><?php esc_html_e( 'You may also like', 'polestar' ) ?></h2>
 
 		<?php woocommerce_product_loop_start(); ?>
 
-			<?php while ( $products->have_posts() ) : $products->the_post(); ?>
+			<?php foreach ( $upsells as $upsell ) : ?>
 
-				<?php wc_get_template_part( 'content', 'product' ); ?>
+				<?php
+				 	$post_object = get_post( $upsell->get_id() );
+					setup_postdata( $GLOBALS['post'] =& $post_object );
+					wc_get_template_part( 'content', 'product' ); ?>
 
-			<?php endwhile; // end of the loop. ?>
+			<?php endforeach; ?>
 
 		<?php woocommerce_product_loop_end(); ?>
 
-	</div>
+	</section>
 
 <?php endif;
-
 wp_reset_postdata();
