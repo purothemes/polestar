6f9a4f78ce5588497f7c08f661b7bd4c1759dc97
Andrew Misplon <amisplon@gmail.com>
2017-04-05 10:34:33 +0200

    Delete about page free partial

--- admin/about/page-free.php
+++
@@ -1,5 +0,0 @@
-<h3><?php esc_html_e( 'Forever Free', 'polestar' ); ?></h3>
-<p>
-	<?php esc_html_e( "Polestar is a completely free WordPress theme.", 'polestar' ); ?>
-	<?php esc_html_e( "We'll continue developing and enhancing it for years to come.", 'polestar' ); ?>
-</p>
