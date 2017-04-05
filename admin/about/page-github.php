651eb8ed599a897b02619408513fd450cf70b69d
Andrew Misplon <amisplon@gmail.com>
2017-04-05 10:34:47 +0200

    Update about page text

--- admin/about/page-github.php
+++ admin/about/page-github.php
@@ -1,6 +1,6 @@
-<h3><?php esc_html_e( 'Developed on Github', 'polestar' ); ?></h3>
+<h3><?php esc_html_e( 'Actively Developed on Github', 'polestar' ); ?></h3>
 <img src="<?php echo get_template_directory_uri() ?>/admin/about/github.png" class="about-image-right about-image-no-text-below" />
 <p>
 	<?php printf( esc_html__( "Polestar is actively %sdeveloped on Github%s.", 'polestar' ), '<a href="https://github.com/purothemes/polestar">', '</a>' ); ?>
-	<?php esc_html_e( "You can watch what's happening with its development and even make contributions yourself.", 'polestar' ); ?>
+	<?php esc_html_e( "You can follow commits, open issues and make contributions yourself.", 'polestar' ); ?>
 </p>
--- admin/about/page-woocommerce.php
+++ admin/about/page-woocommerce.php
@@ -2,5 +2,5 @@
 <img src="<?php echo get_template_directory_uri() ?>/admin/about/woocommerce.png" class="about-image-right about-image-no-text-below" />
 <p>
 	<?php esc_html_e( "Polestar is a full-featured WooCommerce theme.", 'polestar' ) ?>
-	<?php esc_html_e( "Use it to build your full online store with a stunning design and features like quick-view.", 'polestar' ) ?>
+	<?php esc_html_e( "Featuring completely custom WooCommerce styling, keeping your store fast and looking good.", 'polestar' ) ?>
 </p>
