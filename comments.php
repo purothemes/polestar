35c55a130e137f63bd90c985d42ee9c8a21a1152
Andrew Misplon <amisplon@gmail.com>
2017-09-28 09:09:23 +0200

    Update comments nav

--- comments.php
+++ comments.php
@@ -35,15 +35,13 @@ if ( post_password_required() ) {
 			?>
 		</h2>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'polestar' ); ?></h2>
-			<div class="nav-links">
-				<div class="nav-previous"><?php previous_comments_link( '<span class="icon-long-arrow-left"></span> ' . esc_html__( 'Older comments', 'polestar' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer comments', 'polestar' ) . ' <span class="icon-long-arrow-right"></span>' ); ?></div>
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation. ?>
+		<?php 
+		$args = array(
+			'prev_text' => '<span class="icon-long-arrow-left"></span> ' . esc_html__( 'Older comments', 'polestar' ),
+			'next_text' => esc_html__( 'Newer comments', 'polestar' ) . ' <span class="icon-long-arrow-right"></span>',
+		);
+		the_comments_navigation( $args ); 
+		?>
 
 		<ol class="comment-list">
 			<?php
@@ -55,19 +53,12 @@ if ( post_password_required() ) {
 			?>
 		</ol><!-- .comment-list -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'polestar' ); ?></h2>
-			<div class="nav-links">
-				<div class="nav-previous"><?php previous_comments_link( '<span class="icon-long-arrow-left"></span> ' . esc_html__( 'Older comments', 'polestar' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer comments', 'polestar' ) . ' <span class="icon-long-arrow-right"></span>' ); ?></div>
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-below -->
-		<?php
-		endif; // Check for comment navigation.
-
-	endif; // Check for have_comments().
-
+		<?php 
+			$args = array(
+				'prev_text' => '<span class="icon-long-arrow-left"></span> ' . esc_html__( 'Older comments', 'polestar' ),
+				'next_text' => esc_html__( 'Newer comments', 'polestar' ) . ' <span class="icon-long-arrow-right"></span>',
+			);
+			the_comments_navigation( $args ); 
 
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
