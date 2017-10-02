1b645c0f84c9866a617dd67c37ab062ec2ebfc0c
Andrew Misplon <amisplon@gmail.com>
2017-10-02 16:46:13 +0200

    JS spacing for WP

--- js/jquery.theme.js
+++ js/jquery.theme.js
@@ -11,8 +11,8 @@ jQuery( function( $ ) {
 		return (
 			rect.bottom >= 0 &&
 			rect.right >= 0 &&
-			rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-			rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+			rect.top <= ( window.innerHeight || document.documentElement.clientHeight ) &&
+			rect.left <= ( window.innerWidth || document.documentElement.clientWidth )
 		);
 	};	
 
