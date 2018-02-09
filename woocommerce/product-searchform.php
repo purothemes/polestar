b13d9ab14dba818bc38c40b4c3238ac34f333532
Andrew Misplon <amisplon@gmail.com>
2018-02-09 21:40:27 +0200

    Fix text domain

--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <form method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
 	<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>"><?php esc_html_e( 'Search for:', 'polestar' ); ?></label>
 	<input type="search" id="woocommerce-product-search-field" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'polestar' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'polestar' ); ?>" />
-	<button type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>">
+	<button type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'polestar' ); ?>">
 		<label class="screen-reader-text"><?php esc_html_e( 'Search', 'polestar' ); ?></label>
 		<?php polestar_display_icon( 'search' ); ?>
 	</button>
