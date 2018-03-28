07bbb595c1ce7891bc8d3efcc731905401d35f67
Andrew Misplon <amisplon@gmail.com>
2018-03-28 14:25:30 +0200

    Update searchform URL

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" class="search-form" action="<?php echo esc_url( site_url() ) ?>">
+<form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 	<label for='s' class='screen-reader-text'><?php esc_html_e( 'Search for:', 'polestar' ); ?></label>
 	<input type="search" name="s" placeholder="<?php esc_attr_e( 'Search', 'polestar') ?>" value="<?php echo get_search_query() ?>" />
 	<button type="submit">
