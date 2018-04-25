5640457265bcfa7a72599dddc583fa2cf5708221
Andrew Misplon <amisplon@gmail.com>
2018-04-25 15:00:46 +0200

    PHP formatting for WP

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -378,8 +378,7 @@ function polestar_display_logo() {
 		if ( get_theme_mod( 'tagline' ) && get_bloginfo( 'description' ) ) : ?>
 			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 		<?php endif;
-	}
-	else {
+	} else {
 		if ( is_front_page() ) : ?>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php if ( get_theme_mod( 'tagline' ) && get_bloginfo( 'description' ) ) : ?>
