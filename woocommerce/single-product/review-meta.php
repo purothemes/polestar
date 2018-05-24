05e14f90dd57d062f3ed6f711286aab4f10c70d6
Andrew Misplon <amisplon@gmail.com>
2018-05-24 19:18:53 +0200

    Update WooCommerce single review meta file

--- woocommerce/single-product/review-meta.php
+++ woocommerce/single-product/review-meta.php
@@ -1,29 +1,38 @@
 <?php
 /**
- * The template to display the reviewers meta data (name, verified owner, review date).
+ * The template to display the reviewers meta data (name, verified owner, review date)
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.0.0
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/review-meta.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates
+ * @version 3.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+defined( 'ABSPATH' ) || exit;
 
 global $comment;
 $verified = wc_review_is_from_verified_owner( $comment->comment_ID );
 
 if ( '0' === $comment->comment_approved ) { ?>
 
-	<p class="meta"><em><?php esc_attr_e( 'Your comment is awaiting approval', 'polestar' ); ?></em></p>
+	<p class="meta">
+		<em class="woocommerce-review__awaiting-approval">
+			<?php esc_html_e( 'Your review is awaiting approval', 'polestar' ); ?>
+		</em>
+	</p>
 
 <?php } else { ?>
 
 	<p class="meta">
-		<span itemprop="author"><?php comment_author(); ?></span> <?php
-
+		<strong class="woocommerce-review__author"><?php comment_author(); ?> </strong>
+		<?php
 		if ( 'yes' === get_option( 'woocommerce_review_rating_verification_label' ) && $verified ) {
 			echo '<em class="verified">(' . esc_attr__( 'verified owner', 'polestar' ) . ')</em> ';
 		}
