efceabf97da1751135f4cbf3481bfeaf8c36c1b6
Andrew Misplon <amisplon@gmail.com>
2018-10-31 19:21:09 +0200

    Updated WooCommerce templates to 3.5.0.

--- woocommerce/single-product-reviews.php
+++ woocommerce/single-product-reviews.php
@@ -13,7 +13,7 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.2.0
+ * @version     3.5.0
  */
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
@@ -38,8 +38,8 @@ if ( ! comments_open() ) {
 			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
 				echo '<nav class="woocommerce-pagination">';
 				paginate_comments_links( apply_filters( 'woocommerce_comment_pagination_args', array(
-					'prev_text' => '<span class="icon-long-arrow-left"></span>', 
-					'next_text' => '<span class="icon-long-arrow-right"></span>', 
+					'prev_text' => '<span class="icon-long-arrow-left"></span>',
+					'next_text' => '<span class="icon-long-arrow-right"></span>',
 					'type'      => 'list',
 				) ) );
 				echo '</nav>';
@@ -47,7 +47,7 @@ if ( ! comments_open() ) {
 
 		<?php else : ?>
 
-			<p class="woocommerce-noreviews"><?php esc_html_e( 'There are no reviews yet.', 'polestar' ); ?></p>
+			<p class="woocommerce-noreviews"><?php _e( 'There are no reviews yet.', 'polestar' ); ?></p>
 
 		<?php endif; ?>
 	</div>
@@ -60,36 +60,38 @@ if ( ! comments_open() ) {
 					$commenter = wp_get_current_commenter();
 
 					$comment_form = array(
-						'title_reply'          => have_comments() ? esc_html__( 'Add a review', 'polestar' ) : sprintf( esc_html__( 'Be the first to review &ldquo;%s&rdquo;', 'polestar' ), get_the_title() ),
-						'title_reply_to'       => esc_html__( 'Leave a Reply to %s', 'polestar' ),
+						'title_reply'          => have_comments() ? __( 'Add a review', 'polestar' ) : sprintf( __( 'Be the first to review &ldquo;%s&rdquo;', 'polestar' ), get_the_title() ),
+						'title_reply_to'       => __( 'Leave a Reply to %s', 'polestar' ),
+						'title_reply_before'   => '<span id="reply-title" class="comment-reply-title">',
+						'title_reply_after'    => '</span>',
 						'comment_notes_after'  => '',
 						'fields'               => array(
-							'author' => '<p class="comment-form-author">' . '<label for="author">' . esc_html__( 'Name', 'polestar' ) . ' <span class="required">*</span></label> ' .
-										'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" aria-required="true" required /></p>',
-							'email'  => '<p class="comment-form-email"><label for="email">' . esc_html__( 'Email', 'polestar' ) . ' <span class="required">*</span></label> ' .
-										'<input id="email" name="email" type="email" value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30" aria-required="true" required /></p>',
+							'author' => '<p class="comment-form-author">' . '<label for="author">' . esc_html__( 'Name', 'polestar' ) . '&nbsp;<span class="required">*</span></label> ' .
+										'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" required /></p>',
+							'email'  => '<p class="comment-form-email"><label for="email">' . esc_html__( 'Email', 'polestar' ) . '&nbsp;<span class="required">*</span></label> ' .
+										'<input id="email" name="email" type="email" value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30" required /></p>',
 						),
-						'label_submit'  => esc_html__( 'Submit', 'polestar' ),
+						'label_submit'  => __( 'Submit', 'polestar' ),
 						'logged_in_as'  => '',
 						'comment_field' => '',
 					);
 
 					if ( $account_page_url = wc_get_page_permalink( 'myaccount' ) ) {
-						$comment_form['must_log_in'] = '<p class="must-log-in">' . sprintf( esc_html__( 'You must be <a href="%s">logged in</a> to post a review.', 'polestar' ), esc_url( $account_page_url ) ) . '</p>';
+						$comment_form['must_log_in'] = '<p class="must-log-in">' . sprintf( __( 'You must be <a href="%s">logged in</a> to post a review.', 'polestar' ), esc_url( $account_page_url ) ) . '</p>';
 					}
 
 					if ( get_option( 'woocommerce_enable_review_rating' ) === 'yes' ) {
-						$comment_form['comment_field'] = '<div class="comment-form-rating"><label for="rating">' . esc_html__( 'Your Rating', 'polestar' ) .'</label><select name="rating" id="rating" aria-required="true" required>
+						$comment_form['comment_field'] = '<div class="comment-form-rating"><label for="rating">' . esc_html__( 'Your rating', 'polestar' ) . '</label><select name="rating" id="rating" required>
 							<option value="">' . esc_html__( 'Rate&hellip;', 'polestar' ) . '</option>
 							<option value="5">' . esc_html__( 'Perfect', 'polestar' ) . '</option>
 							<option value="4">' . esc_html__( 'Good', 'polestar' ) . '</option>
 							<option value="3">' . esc_html__( 'Average', 'polestar' ) . '</option>
 							<option value="2">' . esc_html__( 'Not that bad', 'polestar' ) . '</option>
-							<option value="1">' . esc_html__( 'Very Poor', 'polestar' ) . '</option>
+							<option value="1">' . esc_html__( 'Very poor', 'polestar' ) . '</option>
 						</select></div>';
 					}
 
-					$comment_form['comment_field'] .= '<p class="comment-form-comment"><label for="comment">' . esc_html__( 'Your Review', 'polestar' ) . ' <span class="required">*</span></label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true" required></textarea></p>';
+					$comment_form['comment_field'] .= '<p class="comment-form-comment"><label for="comment">' . esc_html__( 'Your review', 'polestar' ) . '&nbsp;<span class="required">*</span></label><textarea id="comment" name="comment" cols="45" rows="8" required></textarea></p>';
 
 					comment_form( apply_filters( 'woocommerce_product_review_comment_form_args', $comment_form ) );
 				?>
@@ -98,7 +100,7 @@ if ( ! comments_open() ) {
 
 	<?php else : ?>
 
-		<p class="woocommerce-verification-required"><?php esc_html_e( 'Only logged in customers who have purchased this product may leave a review.', 'polestar' ); ?></p>
+		<p class="woocommerce-verification-required"><?php _e( 'Only logged in customers who have purchased this product may leave a review.', 'polestar' ); ?></p>
 
 	<?php endif; ?>
 
