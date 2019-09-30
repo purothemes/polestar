8586d07869bf900870c3f3d221d507e2917fad52
Andrew Misplon <amisplon@gmail.com>
2019-09-30 14:20:43 +0200

    Open about links in new tab if outside domain

--- admin/about/page-page-builder.php
+++ admin/about/page-page-builder.php
@@ -1,5 +1,5 @@
 <h3><?php esc_html_e( 'Enhanced by Page Builder', 'polestar' ); ?></h3>
 <p>
-	<?php printf( esc_html__( "Polestar integrates, beautifully, with %sPage Builder by SiteOrigin%s.", 'polestar' ), '<a href="https://siteorigin.com/page-builder/">', '</a>' ); ?>
+	<?php printf( esc_html__( "Polestar integrates, beautifully, with %sPage Builder by SiteOrigin%s.", 'polestar' ), '<a href="https://siteorigin.com/page-builder/" target="_blank">', '</a>' ); ?>
 	<?php esc_html_e( 'This powerful plugin gives you full drag and drop capabilities right inside Polestar.', 'polestar' ); ?>
 </p>
