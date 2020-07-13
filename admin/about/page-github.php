ae139e7cc3f3f5872f49a5057458a2b476b51c7a
Andrew Misplon <amisplon@gmail.com>
2020-07-13 17:06:00 +0200

    Escaped URL’s

--- admin/about/page-github.php
+++ admin/about/page-github.php
@@ -1,5 +1,5 @@
 <h3><?php esc_html_e( 'Actively Developed on Github', 'polestar' ); ?></h3>
-<img src="<?php echo get_template_directory_uri() ?>/admin/about/github.png" class="about-image-right about-image-no-text-below" />
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/admin/about/github.png" class="about-image-right about-image-no-text-below" />
 <p>
 	<?php printf( esc_html__( "Polestar is actively %sdeveloped on Github%s.", 'polestar' ), '<a href="https://github.com/purothemes/polestar" target="_blank">', '</a>' ); ?>
 	<?php esc_html_e( "You can follow commits, open issues and make contributions yourself.", 'polestar' ); ?>
--- admin/about/page-woocommerce.php
+++ admin/about/page-woocommerce.php
@@ -1,5 +1,5 @@
 <h3><?php esc_html_e( 'WooCommerce Integration', 'polestar' ) ?></h3>
-<img src="<?php echo get_template_directory_uri() ?>/admin/about/woocommerce.png" class="about-image-right about-image-no-text-below" />
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/admin/about/woocommerce.png" class="about-image-right about-image-no-text-below" />
 <p>
 	<?php esc_html_e( "Polestar is a full-featured WooCommerce theme.", 'polestar' ) ?>
 	<?php esc_html_e( "Featuring premium features like a menu mini-cart and product Quick View. Polestar offers custom WooCommerce styling, keeping your store fast and looking good.", 'polestar' ) ?>
